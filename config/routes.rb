a8e70b2151ad27c67755e85f6b7f6a42f73350b1
nikopapa81 <nikopapa81@gmail.com>
2014-05-05 13:12:50 -0500

    Final Submit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 AddressToCoords::Application.routes.draw do
   get("/coords", { :controller => "addresses", :action => "fetch_coordinates" })
+  get("/coords/:address", { :controller => "addresses", :action => "fetch_coordinates" })
 end
